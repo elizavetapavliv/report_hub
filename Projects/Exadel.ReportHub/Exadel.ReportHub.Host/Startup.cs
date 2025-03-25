c7524934bad49ed1e9084754769b0de1cd15103b
Makar <Makar@DESKTOP-NQ4M3CK>
2025-03-25 12:09:54 +0100

    Setup project

---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace Exadel.ReportHub.Host
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ReportHubAPI"));
+
+            app.UseRouting();
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
