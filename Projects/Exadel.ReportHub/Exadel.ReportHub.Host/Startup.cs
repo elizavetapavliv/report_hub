214196bad4eed5b4d38514f747b2b94ab0a3057c
Makar <Makar@DESKTOP-NQ4M3CK>
2025-03-26 16:25:43 +0100

    Mongo script for creating collection and filling it with seed data

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Startup.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Startup.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.OpenApi.Models;
+﻿using Exadel.ReportHub.Host.Deployment.Mongo;
+using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
 
 namespace Exadel.ReportHub.Host;
 
 public class Startup
 {
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
@@ -16,14 +25,20 @@ public class Startup
         });
 
         services.AddAuthorization();
+
+        var connectionString = Configuration["MongoDbSettings:ConnectionString"];
+        services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
     }
 
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IMongoClient mongoClient)
     {
         app.UseSwagger();
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Report Hub API"));
 
         app.UseRouting();
+
+        var nameDb = Configuration["MongoDbSettings:DatabaseName"];
+        InvoiceSeeder.ExecuteSeedScriptAsync(mongoClient, nameDb).GetAwaiter().GetResult();
 
         app.UseHttpsRedirection();
         app.UseAuthentication();
