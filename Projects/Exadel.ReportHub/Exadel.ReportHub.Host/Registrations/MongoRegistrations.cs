5b24443a9b0848b98668601d1bcd0db076c092ab
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2025-03-27 21:20:56 +0400

    Registered repository as scoped

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Registrations/MongoRegistrations.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Registrations/MongoRegistrations.cs
@@ -1,4 +1,5 @@
 ﻿using Exadel.ReportHub.RA;
+using Exadel.ReportHub.RA.Interfaces;
 
 namespace Exadel.ReportHub.Host.Registrations;
 
@@ -7,6 +8,7 @@ public static class MongoRegistrations
     public static IServiceCollection AddMongo(this IServiceCollection services)
     {
         services.AddSingleton<MongoDbContext>();
+        services.AddScoped<IUserRepository, UserRepository>();
         return services;
     }
 }
