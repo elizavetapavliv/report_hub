04d98d8490b2ae7eb320055ea200d659f5c1607d
Dmitry917 <rogowdmitriy917@gmail.com>
2025-03-28 18:21:50 +0300

    Added ures seed script

---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Deployment/Mongo/03_create_User.js
@@ -0,0 +1,45 @@
+﻿const scriptName = "03_create_User";
+const version = NumberInt(1);
+const demoPassword = "Pa$$word"
+
+if (db.MigrationHistory.findOne({ scriptName, version })) {
+    print(`${scriptName} v${version} is already applied`);
+    quit();
+}
+
+db.createCollection("User", {
+    collation: {
+        locale: "en"
+    }
+});
+
+db.User.InsertMany([
+    {
+        _id: UUID(),
+        Email: "demo.user1@gmail.com",
+        FullName: "Dmitry Rohau",
+        Password: demoPassword,
+        IsActive: false
+    },
+    {
+        _id: UUID(),
+        Email: "demo.user2@gmail.com",
+        FullName: "Makar Kniazeu",
+        Password: demoPassword,
+        IsActive: false
+    },
+    {
+        _id: UUID(),
+        Email: "demo.user3@gmail.com",
+        FullName: "Giorgi Chekurishvili",
+        Password: demoPassword,
+        IsActive: true
+    },
+    {
+        _id: UUID(),
+        Email: "demo.user4@gmail.com",
+        FullName: "Tengo Giorgadze",
+        Password: demoPassword,
+        IsActive: true
+    }
+]);
