52f447879fb060f0c2820addba8465091abfea00
TUGUNA25 <giorgadzetuguna@gmail.com>
2025-03-30 22:13:57 +0400

    Identity resources generic repository

---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.RA/IdentityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Exadel.ReportHub.Data.Models;
+using Exadel.ReportHub.RA.Abstract;
+
+namespace Exadel.ReportHub.RA
+{
+    public class IdentityRepository<TDocument> : BaseRepository<TDocument>, IIdentityRepository<TDocument>
+        where TDocument : IDocument
+    {
+        public IdentityRepository(MongoDbContext context)
+            : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<TDocument>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return await base.GetAllAsync(cancellationToken);
+        }
+
+        public async Task<TDocument> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return await base.GetByIdAsync(id, cancellationToken);
+        }
+    }
+}
