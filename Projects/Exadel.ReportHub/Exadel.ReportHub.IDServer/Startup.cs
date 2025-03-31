21f4901dd335bbe9f4729cdb1d1256944c41d1d4
Dmitry917 <rogowdmitriy917@gmail.com>
2025-03-31 20:08:46 +0300

    Added Identity server project

---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.IDServer/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace Exadel.ReportHub.IDServer;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddAuthentication();
+        services.AddAuthorization();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseRouting();
+
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        //app.UseEndpoints(endpoints =>
+        //{
+        //    endpoints.MapControllers();
+        //});
+    }
+}
