16f22e4428a2545f8e38caeb9f2376729bb742dc
Makar <Makar@DESKTOP-NQ4M3CK>
2025-04-01 14:08:22 +0200

    Added UpdateUserRole endpoint

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Data/Models/User.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Data/Models/User.cs
@@ -14,7 +14,7 @@ public class User : IDocument
 
     public string PasswordSalt { get; set; }
 
-    public UserRole Role { get; set; }
+    public UserRole Role { get; set; } = UserRole.Regular;
 
     public bool IsActive { get; set; } = true;
 }
