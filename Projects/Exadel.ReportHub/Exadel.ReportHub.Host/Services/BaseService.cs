2d3c14a83d8729b2d3926a7b0597b0b2ff3a32f8
Makar <Makar@DESKTOP-NQ4M3CK>
2025-04-02 11:31:21 +0200

    Small Fixes

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Services/BaseService.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Services/BaseService.cs
@@ -39,46 +39,25 @@ public abstract class BaseService : ControllerBase
 
     private IActionResult GetErrorResult(List<Error> errors)
     {
-        if (errors.Count == 0)
-        {
-            return new ObjectResult(new ErrorResponse())
-            {
-                StatusCode = StatusCodes.Status500InternalServerError
-            };
-        }
-
         var errorResponse = new ErrorResponse
         {
             Errors = errors.Select(e => e.Description).ToList()
         };
+        var statusCode = StatusCodes.Status500InternalServerError;
 
         if (errors.Any(e => e.Type == ErrorType.Validation))
         {
-            return new ObjectResult(errorResponse)
-            {
-                StatusCode = StatusCodes.Status400BadRequest
-            };
+            statusCode = StatusCodes.Status400BadRequest;
+        }
+        else if(errors.Any(e => e.Type == ErrorType.Forbidden))
+        {
+            statusCode = StatusCodes.Status403Forbidden;
+        }
+        else if (errors.Any(e => e.Type == ErrorType.NotFound))
+        {
+            statusCode = StatusCodes.Status404NotFound;
         }
 
-        if (errors.Any(e => e.Type == ErrorType.Forbidden))
-        {
-            return new ObjectResult(errorResponse)
-            {
-                StatusCode = StatusCodes.Status403Forbidden
-            };
-        }
-
-        if (errors.Any(e => e.Type == ErrorType.NotFound))
-        {
-            return new ObjectResult(errorResponse)
-            {
-                StatusCode = StatusCodes.Status404NotFound
-            };
-        }
-
-        return new ObjectResult(errorResponse)
-        {
-            StatusCode = StatusCodes.Status500InternalServerError
-        };
+        return StatusCode(statusCode, errorResponse);
     }
 }
