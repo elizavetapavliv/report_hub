715283aab626fcf40a746920236c6b0d15108788
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2025-04-04 16:48:44 +0400

    Update password of authenticated user #43

---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/User/UpdatePassword/UpdateUserPasswordRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using System.Security;
+using FluentValidation;
+
+namespace Exadel.ReportHub.Handlers.User.UpdatePassword;
+
+public class UpdateUserPasswordRequestValidator : AbstractValidator<UpdateUserPasswordRequest>
+{
+    private readonly IValidator<string> _passwordValidator;
+
+    public UpdateUserPasswordRequestValidator(IValidator<string> passwordValidator)
+    {
+        _passwordValidator = passwordValidator;
+        ConfigureRules();
+    }
+
+    private void ConfigureRules()
+    {
+        RuleFor(x => x.Password)
+            .SetValidator(_passwordValidator);
+    }
+}
