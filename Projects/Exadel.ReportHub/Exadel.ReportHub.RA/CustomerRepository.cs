563264f9a4fe81f040aef725b957be5675702708
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2025-04-09 23:28:38 +0400

    Update as requested

---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.RA/CustomerRepository.cs
@@ -0,0 +1,22 @@
+﻿using Exadel.ReportHub.Data.Models;
+using Exadel.ReportHub.RA.Abstract;
+using MongoDB.Driver;
+
+namespace Exadel.ReportHub.RA;
+
+public class CustomerRepository : BaseRepository, ICustomerRepository
+{
+    private static readonly FilterDefinitionBuilder<Customer> _filterBuilder = Builders<Customer>.Filter;
+
+    public CustomerRepository(MongoDbContext context)
+        : base(context)
+    {
+    }
+
+    public async Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var filter = _filterBuilder.Eq(x => x.Id, id);
+        var count = await GetCollection<Customer>().Find(filter).CountDocumentsAsync(cancellationToken);
+        return count > 0;
+    }
+}
