e93b0d8e5f51ea6fe6f18d3f5ab480a54754b9e0
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2025-04-14 18:32:52 +0400

    Added User service: delete endpoint, update name. Modified active users endpoint, Added unit tests,  Added UpdateUserNameRequestValidator.

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/User/GetActive/GetActiveUsersHandler.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/User/GetActive/GetActiveUsersHandler.cs
@@ -7,13 +7,13 @@ using MediatR;
 
 namespace Exadel.ReportHub.Handlers.User.GetActive;
 
-public record GetActiveUsersRequest : IRequest<ErrorOr<IEnumerable<UserDTO>>>;
+public record GetActiveUsersRequest(bool? isActive) : IRequest<ErrorOr<IEnumerable<UserDTO>>>;
 
 public class GetActiveUsersHandler(IUserRepository userRepository, IMapper mapper) : IRequestHandler<GetActiveUsersRequest, ErrorOr<IEnumerable<UserDTO>>>
 {
     public async Task<ErrorOr<IEnumerable<UserDTO>>> Handle(GetActiveUsersRequest request, CancellationToken cancellationToken)
     {
-        var users = await userRepository.GetAllActiveAsync(cancellationToken);
+        var users = await userRepository.GetAllActiveAsync(request.isActive, cancellationToken);
 
         var userDtos = mapper.Map<List<UserDTO>>(users);
 
---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Tests/User/UpdateName/UpdateUserNameHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using ErrorOr;
+using Exadel.ReportHub.Common.Providers;
+using Exadel.ReportHub.Handlers.User.UpdateName;
+using Exadel.ReportHub.RA.Abstract;
+using Moq;
+
+namespace Exadel.ReportHub.Tests;
+
+public class UpdateUserNameHandlerTests
+{
+    private Mock<IUserRepository> _userRepositoryMock;
+    private Mock<IUserProvider> _userProviderMock;
+    private UpdateUserNameHandler _handler;
+
+    [SetUp]
+    public void Setup()
+    {
+        _userRepositoryMock = new Mock<IUserRepository>();
+        _userProviderMock = new Mock<IUserProvider>();
+        _handler = new UpdateUserNameHandler(_userRepositoryMock.Object, _userProviderMock.Object);
+    }
+
+    [Test]
+    public async Task UpdateUserName_ValidRequest_ReturnsUpdated()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+        var newName = "New Name";
+        var request = new UpdateUserNameRequest(newName);
+        _userProviderMock
+            .Setup(x => x.GetUserId())
+            .Returns(userId);
+        _userRepositoryMock
+            .Setup(x => x.UpdateNameAsync(userId, newName, CancellationToken.None))
+            .Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        Assert.That(result.IsError, Is.False);
+        Assert.That(result.Value, Is.EqualTo(Result.Updated));
+
+        _userProviderMock.Verify(x => x.GetUserId(), Times.Once);
+        _userRepositoryMock.Verify(x => x.UpdateNameAsync(userId, newName, CancellationToken.None), Times.Once);
+    }
+}
