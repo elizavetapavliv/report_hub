1f6809dbd963f7939b082436b07ee7e312ed8f03
Dmitry917 <rogowdmitriy917@gmail.com>
2025-04-18 13:48:08 +0300

    Fixed as required

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/Item/Create/CreateItemRequestValidator.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/Item/Create/CreateItemRequestValidator.cs
@@ -1,19 +1,15 @@
-﻿using Exadel.ReportHub.RA.Abstract;
+﻿using Exadel.ReportHub.SDK.DTOs.Item;
 using FluentValidation;
 
 namespace Exadel.ReportHub.Handlers.Item.Create;
 
 public class CreateItemRequestValidator : AbstractValidator<CreateItemRequest>
 {
-    private readonly ICurrencyRepository _currencyRepository;
-    private readonly IClientRepository _clientRepository;
-    private readonly IValidator<string> _stringValidator;
+    private readonly IValidator<CreateUpdateItemDTO> _itemValidator;
 
-    public CreateItemRequestValidator(ICurrencyRepository currencyRepository, IClientRepository clientRepository, IValidator<string> stringValidator)
+    public CreateItemRequestValidator(IValidator<CreateUpdateItemDTO> itemValidator)
     {
-        _currencyRepository = currencyRepository;
-        _clientRepository = clientRepository;
-        _stringValidator = stringValidator;
+        _itemValidator = itemValidator;
         ConfigureRules();
     }
 
@@ -22,33 +18,6 @@ public class CreateItemRequestValidator : AbstractValidator<CreateItemRequest>
         RuleLevelCascadeMode = CascadeMode.Stop;
 
         RuleFor(x => x.CreateItemDto)
-            .ChildRules(child =>
-            {
-                child.RuleLevelCascadeMode = CascadeMode.Stop;
-
-                child.RuleFor(x => x.ClientId)
-                    .NotEmpty()
-                    .MustAsync(_clientRepository.ExistsAsync)
-                    .WithMessage(Constants.Validation.Item.ClientDoesNotExistMessage);
-
-                child.RuleFor(x => x.CurrencyId)
-                    .NotEmpty()
-                    .MustAsync(_currencyRepository.ExistsAsync)
-                    .WithMessage(Constants.Validation.Item.CurrencyDoesNotExistMessage);
-
-                child.RuleFor(x => x.Name)
-                    .SetValidator(_stringValidator, Constants.Validation.RuleSet.Names);
-
-                child.RuleFor(x => x.Description)
-                    .NotEmpty()
-                    .MaximumLength(Constants.Validation.Item.DescriptionMaxLength)
-                    .Matches("^[A-Z]")
-                    .WithMessage(Constants.Validation.Item.DescriptionShouldStartWithCapitalMessage);
-
-                child.RuleFor(x => x.Price)
-                    .NotEmpty()
-                    .GreaterThan(0)
-                    .WithMessage(Constants.Validation.Item.NegativePriceErrorMessage);
-            });
+            .SetValidator(_itemValidator);
     }
 }
--- Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/Item/Update/UpdateItemRequestValidator.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/Item/Update/UpdateItemRequestValidator.cs
@@ -1,19 +1,15 @@
-﻿using Exadel.ReportHub.RA.Abstract;
+﻿using Exadel.ReportHub.SDK.DTOs.Item;
 using FluentValidation;
 
 namespace Exadel.ReportHub.Handlers.Item.Update;
 
 public class UpdateItemRequestValidator : AbstractValidator<UpdateItemRequest>
 {
-    private readonly ICurrencyRepository _currencyRepository;
-    private readonly IClientRepository _clientRepository;
-    private readonly IValidator<string> _stringValidator;
+    private readonly IValidator<CreateUpdateItemDTO> _itemValidator;
 
-    public UpdateItemRequestValidator(ICurrencyRepository currencyRepository, IClientRepository clientRepository, IValidator<string> stringValidator)
+    public UpdateItemRequestValidator(IValidator<CreateUpdateItemDTO> itemValidator)
     {
-        _currencyRepository = currencyRepository;
-        _clientRepository = clientRepository;
-        _stringValidator = stringValidator;
+        _itemValidator = itemValidator;
         ConfigureRules();
     }
 
@@ -22,33 +18,6 @@ public class UpdateItemRequestValidator : AbstractValidator<UpdateItemRequest>
         RuleLevelCascadeMode = CascadeMode.Stop;
 
         RuleFor(x => x.UpdateItemDTO)
-            .ChildRules(child =>
-            {
-                child.RuleLevelCascadeMode = CascadeMode.Stop;
-
-                child.RuleFor(x => x.ClientId)
-                    .NotEmpty()
-                    .MustAsync(_clientRepository.ExistsAsync)
-                    .WithMessage(Constants.Validation.Item.ClientDoesNotExistMessage);
-
-                child.RuleFor(x => x.CurrencyId)
-                    .NotEmpty()
-                    .MustAsync(_currencyRepository.ExistsAsync)
-                    .WithMessage(Constants.Validation.Item.CurrencyDoesNotExistMessage);
-
-                child.RuleFor(x => x.Name)
-                    .SetValidator(_stringValidator, Constants.Validation.RuleSet.Names);
-
-                child.RuleFor(x => x.Description)
-                    .NotEmpty()
-                    .MaximumLength(Constants.Validation.Item.DescriptionMaxLength)
-                    .Matches("^[A-Z]")
-                    .WithMessage(Constants.Validation.Item.DescriptionShouldStartWithCapitalMessage);
-
-                child.RuleFor(x => x.Price)
-                    .NotEmpty()
-                    .GreaterThan(0)
-                    .WithMessage(Constants.Validation.Item.NegativePriceErrorMessage);
-            });
+            .SetValidator(_itemValidator);
     }
 }
