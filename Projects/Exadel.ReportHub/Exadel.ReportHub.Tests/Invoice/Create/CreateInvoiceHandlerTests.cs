865a192423b07fb529604f5ea243ed8b4f40d370
Makar <Makar@DESKTOP-NQ4M3CK>
2025-04-24 10:39:35 +0200

    Invoice Tests

---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Tests/Invoice/Create/CreateInvoiceHandlerTests.cs
@@ -0,0 +1,52 @@
+﻿using AutoFixture;
+using Exadel.ReportHub.Handlers.Invoice.Create;
+using Exadel.ReportHub.RA.Abstract;
+using Exadel.ReportHub.SDK.DTOs.Invoice;
+using Exadel.ReportHub.Tests.Abstracts;
+using Moq;
+
+namespace Exadel.ReportHub.Tests.Invoice.Create;
+
+[TestFixture]
+public class CreateInvoiceHandlerTests : BaseTestFixture
+{
+    private Mock<IInvoiceRepository> _invoiceRepositoryMock;
+    private CreateInvoiceHandler _handler;
+
+    [SetUp]
+    public void Setup()
+    {
+        _invoiceRepositoryMock = new Mock<IInvoiceRepository>();
+        _handler = new CreateInvoiceHandler(_invoiceRepositoryMock.Object, Mapper);
+    }
+
+    [Test]
+    public async Task CreateInvoice_ValidRequest_ReturnsInvoiceDto()
+    {
+        // Arrange
+        var createInvoiceDto = Fixture.Create<CreateInvoiceDTO>();
+
+        // Act
+        var result = await _handler.Handle(new CreateInvoiceRequest(createInvoiceDto), CancellationToken.None);
+
+        // Assert
+        Assert.That(result.Value, Is.Not.Null);
+        Assert.That(result.IsError, Is.False);
+        Assert.That(result.Value, Is.InstanceOf<InvoiceDTO>(), "Returned object should be an instance of InvoiceDTO");
+        Assert.That(result.Value.Id, Is.Not.EqualTo(Guid.Empty));
+
+        _invoiceRepositoryMock.Verify(
+            mock => mock.AddAsync(
+                It.Is<Data.Models.Invoice>(
+                    i => i.ClientId == createInvoiceDto.ClientId &&
+                    i.CustomerId == createInvoiceDto.CustomerId &&
+                    i.InvoiceNumber == createInvoiceDto.InvoiceNumber &&
+                    i.IssueDate == createInvoiceDto.IssueDate &&
+                    i.DueDate == createInvoiceDto.DueDate &&
+                    i.PaymentStatus == (Data.Enums.PaymentStatus)createInvoiceDto.PaymentStatus &&
+                    i.BankAccountNumber == createInvoiceDto.BankAccountNumber &&
+                    i.ItemIds.SequenceEqual(createInvoiceDto.ItemIds)),
+                CancellationToken.None),
+            Times.Once);
+    }
+}
---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Tests/Invoice/Delete/DeleteInvoiceHandlerTests.cs
@@ -0,0 +1,60 @@
+﻿using ErrorOr;
+using Exadel.ReportHub.Handlers.Invoice.Delete;
+using Exadel.ReportHub.RA.Abstract;
+using Moq;
+
+namespace Exadel.ReportHub.Tests.Invoice.Delete;
+
+[TestFixture]
+public class DeleteInvoiceHandlerTests
+{
+    private Mock<IInvoiceRepository> _invoiceRepositoryMock;
+    private DeleteInvoiceHandler _handler;
+
+    [SetUp]
+    public void Setup()
+    {
+        _invoiceRepositoryMock = new Mock<IInvoiceRepository>();
+        _handler = new DeleteInvoiceHandler(_invoiceRepositoryMock.Object);
+    }
+
+    [Test]
+    public async Task DeleteInvoice_WhenExists_ReturnsDeleted()
+    {
+        var id = Guid.NewGuid();
+
+        _invoiceRepositoryMock
+            .Setup(r => r.ExistsAsync(id, CancellationToken.None))
+            .ReturnsAsync(true);
+
+        // Act
+        var result = await _handler.Handle(new DeleteInvoiceRequest(id), CancellationToken.None);
+
+        // Assert
+        Assert.That(result.IsError, Is.False);
+        Assert.That(result.Value, Is.EqualTo(Result.Deleted));
+
+        _invoiceRepositoryMock.Verify(r => r.ExistsAsync(id, CancellationToken.None), Times.Once);
+        _invoiceRepositoryMock.Verify(r => r.SoftDeleteAsync(id, CancellationToken.None), Times.Once);
+    }
+
+    [Test]
+    public async Task DeleteInvoice_WhenNotExist_ReturnsNotFound()
+    {
+        var id = Guid.NewGuid();
+
+        _invoiceRepositoryMock
+            .Setup(r => r.ExistsAsync(id, CancellationToken.None))
+            .ReturnsAsync(false);
+
+        // Act
+        var result = await _handler.Handle(new DeleteInvoiceRequest(id), CancellationToken.None);
+
+        // Assert
+        Assert.That(result.Errors, Has.Count.EqualTo(1), "Should contains the only error");
+        Assert.That(result.FirstError.Type, Is.EqualTo(ErrorType.NotFound));
+
+        _invoiceRepositoryMock.Verify(r => r.ExistsAsync(id, CancellationToken.None), Times.Once);
+        _invoiceRepositoryMock.Verify(r => r.SoftDeleteAsync(id, It.IsAny<CancellationToken>()), Times.Never);
+    }
+}
