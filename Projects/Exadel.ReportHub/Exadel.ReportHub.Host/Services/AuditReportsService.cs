2e0a4644369c72a3cea2e407b8d4bcd44124a723
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2025-04-25 01:23:59 +0400

    minor changes

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Services/AuditReportsService.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Services/AuditReportsService.cs
@@ -22,7 +22,6 @@ public class AuditReportsService(ISender sender) : BaseService
     [SwaggerOperation(Summary = "Get audit reports", Description = "Retrieves a list of all Audit reports")]
     [SwaggerResponse(StatusCodes.Status200OK, "Audit reports were retrieved successfully", typeof(ActionResult<IList<AuditReportDTO>>))]
     [SwaggerResponse(StatusCodes.Status401Unauthorized, "Authentication is required to access this endpoint")]
-    [SwaggerResponse(StatusCodes.Status403Forbidden, "This User doesnt have permission to access the Audit reports")]
     [SwaggerResponse(StatusCodes.Status500InternalServerError, type: typeof(ErrorResponse))]
     public async Task<ActionResult<IList<AuditReportDTO>>> GetAuditReports()
     {
@@ -35,7 +34,6 @@ public class AuditReportsService(ISender sender) : BaseService
     [SwaggerOperation(Summary = "Get audit report by id", Description = "Retrieves a specific audit report using their unique identifier")]
     [SwaggerResponse(StatusCodes.Status200OK, "Audit report was retrieved successfully", typeof(ActionResult<ClientDTO>))]
     [SwaggerResponse(StatusCodes.Status401Unauthorized, "Authentication is required to access this endpoint")]
-    [SwaggerResponse(StatusCodes.Status403Forbidden, "This User doesnt have permission to access the Audit report")]
     [SwaggerResponse(StatusCodes.Status404NotFound, "Audit report was not found", typeof(ErrorResponse))]
     [SwaggerResponse(StatusCodes.Status500InternalServerError, type: typeof(ErrorResponse))]
     public async Task<ActionResult<AuditReportDTO>> GetAuditReportById([FromRoute] Guid id)
@@ -49,7 +47,6 @@ public class AuditReportsService(ISender sender) : BaseService
     [SwaggerOperation(Summary = "Get audit reports by user id", Description = "Retrieves a specific audit report using their user's identifier")]
     [SwaggerResponse(StatusCodes.Status200OK, "Audit reports were retrieved successfully", typeof(ActionResult<ClientDTO>))]
     [SwaggerResponse(StatusCodes.Status401Unauthorized, "Authentication is required to access this endpoint")]
-    [SwaggerResponse(StatusCodes.Status403Forbidden, "This User doesnt have permission to access the Audit reports")]
     [SwaggerResponse(StatusCodes.Status404NotFound, "User doesnt exist", typeof(ErrorResponse))]
     [SwaggerResponse(StatusCodes.Status500InternalServerError, type: typeof(ErrorResponse))]
     public async Task<ActionResult<IList<AuditReportDTO>>> GetAuditReportsByUserId([FromRoute] Guid id)
