6e77004cb565f53d9c7e90cbedc11dcbcbd003a1
Makar <Makar@DESKTOP-NQ4M3CK>
2025-04-27 18:15:05 +0200

    update as requested

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/Invoice/ExportPdf/ExportPdfInvoiceHandler.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/Invoice/ExportPdf/ExportPdfInvoiceHandler.cs
@@ -2,7 +2,7 @@
 using AutoMapper;
 using ErrorOr;
 using Exadel.ReportHub.Common.Providers;
-using Exadel.ReportHub.Handlers.Notifications;
+using Exadel.ReportHub.Handlers.Notifications.Invoice.Export;
 using Exadel.ReportHub.Pdf.Abstract;
 using Exadel.ReportHub.Pdf.Models;
 using Exadel.ReportHub.RA.Abstract;
@@ -25,43 +25,51 @@ public class ExportPdfInvoiceHandler(
 {
     public async Task<ErrorOr<ExportResult>> Handle(ExportPdfInvoiceRequest request, CancellationToken cancellationToken)
     {
-        var invoice = await invoiceRepository.GetByIdAsync(request.InvoiceId, cancellationToken);
-        if (invoice is null)
-        {
-            return Error.NotFound();
-        }
-
-        var itemsTask = itemRepository.GetByIdsAsync(invoice.ItemIds, cancellationToken);
-        var clientTask = clientRepository.GetByIdAsync(invoice.ClientId, cancellationToken);
-        var customerTask = customerRepository.GetByIdAsync(invoice.CustomerId, cancellationToken);
-        await Task.WhenAll(itemsTask, clientTask, customerTask);
-
-        var invoiceModel = new InvoiceModel
-        {
-            ClientName = clientTask.Result.Name,
-            CustomerName = customerTask.Result.Name,
-            InvoiceNumber = invoice.InvoiceNumber,
-            IssueDate = invoice.IssueDate,
-            DueDate = invoice.DueDate,
-            Amount = invoice.Amount,
-            CurrencyCode = invoice.CurrencyCode,
-            PaymentStatus = (SDK.Enums.PaymentStatus)invoice.PaymentStatus,
-            ClientBankAccountNumber = invoice.ClientBankAccountNumber,
-            Items = mapper.Map<IList<ItemDTO>>(itemsTask.Result)
-        };
-        var stream = await pdfInvoiceGenerator.GenerateAsync(invoiceModel, cancellationToken);
-
-        var exportDto = new ExportResult
-        {
-            Stream = stream,
-            FileName = $"{Constants.File.Name.Invoice}{invoice.InvoiceNumber}{Constants.File.Extension.Pdf}",
-            ContentType = MediaTypeNames.Application.Pdf
-        };
-
         var userId = userProvider.GetUserId();
-        var notification = new InvoiceExportedNotification(userId, request.InvoiceId);
-        await publisher.Publish(notification, cancellationToken);
+        var isSuccess = false;
 
-        return exportDto;
+        try
+        {
+            var invoice = await invoiceRepository.GetByIdAsync(request.InvoiceId, cancellationToken);
+            if (invoice is null)
+            {
+                return Error.NotFound();
+            }
+
+            var itemsTask = itemRepository.GetByIdsAsync(invoice.ItemIds, cancellationToken);
+            var clientTask = clientRepository.GetByIdAsync(invoice.ClientId, cancellationToken);
+            var customerTask = customerRepository.GetByIdAsync(invoice.CustomerId, cancellationToken);
+            await Task.WhenAll(itemsTask, clientTask, customerTask);
+
+            var invoiceModel = new InvoiceModel
+            {
+                ClientName = clientTask.Result.Name,
+                CustomerName = customerTask.Result.Name,
+                InvoiceNumber = invoice.InvoiceNumber,
+                IssueDate = invoice.IssueDate,
+                DueDate = invoice.DueDate,
+                Amount = invoice.Amount,
+                CurrencyCode = invoice.CurrencyCode,
+                PaymentStatus = (SDK.Enums.PaymentStatus)invoice.PaymentStatus,
+                ClientBankAccountNumber = invoice.ClientBankAccountNumber,
+                Items = mapper.Map<IList<ItemDTO>>(itemsTask.Result)
+            };
+            var stream = await pdfInvoiceGenerator.GenerateAsync(invoiceModel, cancellationToken);
+
+            var exportDto = new ExportResult
+            {
+                Stream = stream,
+                FileName = $"{Constants.File.Name.Invoice}{invoice.InvoiceNumber}{Constants.File.Extension.Pdf}",
+                ContentType = MediaTypeNames.Application.Pdf
+            };
+            isSuccess = true;
+
+            return exportDto;
+        }
+        finally
+        {
+            var notification = new InvoiceExportedNotification(userId, request.InvoiceId, DateTime.UtcNow, isSuccess);
+            await publisher.Publish(notification, cancellationToken);
+        }
     }
 }
---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/Notifications/Invoice/Export/InvoiceExportedNotificationHandler.cs
@@ -0,0 +1,22 @@
+﻿using Exadel.ReportHub.Audit;
+using Exadel.ReportHub.Audit.Abstract;
+using MediatR;
+
+namespace Exadel.ReportHub.Handlers.Notifications.Invoice.Export;
+
+public record InvoiceExportedNotification(Guid UserId, Guid InvoiceId, DateTime TimeStamp, bool IsSuccess) : INotification;
+
+public class AuditInvoiceExportedNotificationHandler(IAuditManager auditManager) : INotificationHandler<InvoiceExportedNotification>
+{
+    public async Task Handle(InvoiceExportedNotification notification, CancellationToken cancellationToken)
+    {
+        var action = new ExportInvoicesAuditAction(
+            userId: notification.UserId,
+            invoiceId: notification.InvoiceId,
+            timeStamp: notification.TimeStamp,
+            isSuccess: notification.IsSuccess
+        );
+
+        await auditManager.AuditAsync(action, cancellationToken);
+    }
+}
