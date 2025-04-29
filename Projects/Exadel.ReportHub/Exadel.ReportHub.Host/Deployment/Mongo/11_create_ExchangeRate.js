0365f0e7a21c43a8f03d75dca2a96f505478a86e
mkknyaz <makar.kniazeu@gmail.com>
2025-04-29 09:52:29 +0200

    Update 11_create_ExchangeRate.js

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Deployment/Mongo/11_create_ExchangeRate.js
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Deployment/Mongo/11_create_ExchangeRate.js
@@ -1,5 +1,5 @@
 ﻿const scriptName = "11_create_ExchangeRate";
-const version = NumberInt(2);
+const version = NumberInt(1);
 
 if (db.MigrationHistory.findOne({ ScriptName: scriptName, Version: version })) {
     print(`${scriptName} v${version} is already applied`);
