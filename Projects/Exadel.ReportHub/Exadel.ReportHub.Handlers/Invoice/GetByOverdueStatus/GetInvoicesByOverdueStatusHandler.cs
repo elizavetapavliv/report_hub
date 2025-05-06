e5417b3495264c582cc8978f3c82665593f967f5
Makar <Makar@DESKTOP-NQ4M3CK>
2025-05-06 14:25:54 +0200

    update

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/Invoice/GetByOverdueStatus/GetInvoicesByOverdueStatusHandler.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/Invoice/GetByOverdueStatus/GetInvoicesByOverdueStatusHandler.cs
@@ -8,11 +8,24 @@ namespace Exadel.ReportHub.Handlers.Invoice.GetByOverdueStatus;
 
 public record GetInvoicesByOverdueStatusRequest(Guid ClientId) : IRequest<ErrorOr<OverdueInvoicesResultDTO>>;
 
-public class GetInvoicesByOverdueStatusHandler(IInvoiceRepository invoiceRepository, IMapper mapper) : IRequestHandler<GetInvoicesByOverdueStatusRequest, ErrorOr<OverdueInvoicesResultDTO>>
+public class GetInvoicesByOverdueStatusHandler(
+    IInvoiceRepository invoiceRepository,
+    IClientRepository clientRepository,
+    IMapper mapper) : IRequestHandler<GetInvoicesByOverdueStatusRequest, ErrorOr<OverdueInvoicesResultDTO>>
 {
     public async Task<ErrorOr<OverdueInvoicesResultDTO>> Handle(GetInvoicesByOverdueStatusRequest request, CancellationToken cancellationToken)
     {
         var overdueResult = await invoiceRepository.GetOverdueAsync(request.ClientId, cancellationToken);
+        if(overdueResult is null)
+        {
+            var emptyOverdueResultDto = new OverdueInvoicesResultDTO
+            {
+                ClientCurrencyCode = await clientRepository.GetCurrencyAsync(request.ClientId, cancellationToken),
+            };
+
+            return emptyOverdueResultDto;
+        }
+
         var overdueResultDto = mapper.Map<OverdueInvoicesResultDTO>(overdueResult);
 
         return overdueResultDto;
--- Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/Invoice/GetRevenue/GetInvoicesRevenueHandler.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Handlers/Invoice/GetRevenue/GetInvoicesRevenueHandler.cs
@@ -8,12 +8,28 @@ namespace Exadel.ReportHub.Handlers.Invoice.GetRevenue;
 
 public record GetInvoicesRevenueRequest(InvoiceRevenueFilterDTO InvoiceRevenueFilterDto) : IRequest<ErrorOr<TotalInvoicesRevenueDTO>>;
 
-public class GetInvoicesRevenueHandler(IInvoiceRepository invoiceRepository, IMapper mapper) : IRequestHandler<GetInvoicesRevenueRequest, ErrorOr<TotalInvoicesRevenueDTO>>
+public class GetInvoicesRevenueHandler(
+    IInvoiceRepository invoiceRepository,
+    IClientRepository clientRepository,
+    IMapper mapper) : IRequestHandler<GetInvoicesRevenueRequest, ErrorOr<TotalInvoicesRevenueDTO>>
 {
     public async Task<ErrorOr<TotalInvoicesRevenueDTO>> Handle(GetInvoicesRevenueRequest request, CancellationToken cancellationToken)
     {
-        var revenueResult = await invoiceRepository.GetTotalAmountByDateRangeAsync(request.InvoiceRevenueFilterDto.ClientId,
-            request.InvoiceRevenueFilterDto.StartDate, request.InvoiceRevenueFilterDto.EndDate, cancellationToken);
+        var revenueResult = await invoiceRepository.GetTotalAmountByDateRangeAsync(
+            request.InvoiceRevenueFilterDto.ClientId,
+            request.InvoiceRevenueFilterDto.StartDate,
+            request.InvoiceRevenueFilterDto.EndDate,
+            cancellationToken);
+        if (revenueResult is null)
+        {
+            var emptyRevenueResultDto = new TotalInvoicesRevenueDTO
+            {
+                CurrencyCode = await clientRepository.GetCurrencyAsync(request.InvoiceRevenueFilterDto.ClientId, cancellationToken)
+            };
+
+            return emptyRevenueResultDto;
+        }
+
         var revenueResultDto = mapper.Map<TotalInvoicesRevenueDTO>(revenueResult);
 
         return revenueResultDto;
