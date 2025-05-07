cbbdc2d3661f6ef57e47a57e6b0f1aa91a00b7cb
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2025-05-07 19:03:46 +0400

    small change

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Deployment/Mongo/22_update_User_AddNotificationSettings.js
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Deployment/Mongo/22_update_User_AddNotificationSettings.js
@@ -6,10 +6,10 @@ if (db.MigrationHistory.findOne({ ScriptName: scriptName, Version: version })) {
     quit();
 }
 
-const Frequencies = ["Daily", "Weekly", "Monthly"];
+const frequencies = ["Daily", "Weekly", "Monthly"];
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const exportFormats = ["Excel", "CSV"];
-const Hours = [8, 9, 10, 11, 14, 15, 16, 17];
+const hours = [8, 9, 10, 11, 14, 15, 16, 17];
 
 const users = db.User.find({}, { _id: 1 }).toArray();
 
@@ -20,11 +20,11 @@ var updates = users.map(user => {
         DayOfMonth: null,
     };
 
-    const randomFrequency = Frequencies[Math.floor(Math.random() * Frequencies.length)];
+    const randomFrequency = frequencies[Math.floor(Math.random() * frequencies.length)];
     notificationSettings.Frequency = randomFrequency;
 
 
-    notificationSettings.Hour = Hours[Math.floor(Math.random() * Hours.length)];
+    notificationSettings.Hour = hours[Math.floor(Math.random() * hours.length)];
 
     if (randomFrequency === "Weekly") {
         notificationSettings.DayOfWeek = daysOfWeek[Math.floor(Math.random() * daysOfWeek.length)]
