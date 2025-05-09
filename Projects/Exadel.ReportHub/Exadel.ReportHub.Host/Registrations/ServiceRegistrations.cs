5e6f24f04dbc3a202e7b7919bcd8bcbe5c8e8b5d
Makar <Makar@DESKTOP-NQ4M3CK>
2025-05-09 12:31:15 +0200

    FIX for Jobs

---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Registrations/ServiceRegistrations.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Exadel.ReportHub.Host.Services;
+using Exadel.ReportHub.SDK.Abstract;
+
+namespace Exadel.ReportHub.Host.Registrations;
+
+[ExcludeFromCodeCoverage]
+public static class ServiceRegistrations
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IInvoiceService, InvoicesService>();
+        services.AddScoped<IReportService, ReportsService>();
+
+        return services;
+    }
+}
