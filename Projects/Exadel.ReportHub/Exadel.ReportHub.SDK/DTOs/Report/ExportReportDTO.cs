22f3e923ea28da5472912060bd451ba1779b1d0b
Dmitry917 <rogowdmitriy917@gmail.com>
2025-05-10 13:22:17 +0300

    Remived required attribute

--- Projects/Exadel.ReportHub/Exadel.ReportHub.SDK/DTOs/Report/ExportReportDTO.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.SDK/DTOs/Report/ExportReportDTO.cs
@@ -1,14 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Exadel.ReportHub.SDK.Enums;
+﻿using Exadel.ReportHub.SDK.Enums;
 
 namespace Exadel.ReportHub.SDK.DTOs.Report;
 
 public class ExportReportDTO
 {
-    [Required]
     public Guid ClientId { get; set; }
 
-    [Required]
     public ExportFormat Format { get; set; }
 
     public DateTime? StartDate { get; set; }
