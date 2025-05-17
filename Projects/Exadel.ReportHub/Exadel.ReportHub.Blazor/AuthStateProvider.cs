5e4ce514843a28792af03002de5b9cd0ff6e7a70
Dmitry917 <rogowdmitriy917@gmail.com>
2025-05-18 02:17:36 +0300

    Added AuthStateProvider

---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Blazor/AuthStateProvider.cs
@@ -0,0 +1,52 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Duende.IdentityModel;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+
+namespace Exadel.ReportHub.Blazor;
+
+public class AuthStateProvider(IJSRuntime js) : AuthenticationStateProvider
+{
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var token = await js.InvokeAsync<string>("sessionStorage.getItem", OidcConstants.TokenResponse.AccessToken);
+
+        if (string.IsNullOrWhiteSpace(token))
+        {
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        }
+
+        var identity = ParseClaimsFromJwt(token);
+        return new AuthenticationState(new ClaimsPrincipal(identity));
+    }
+
+    public void NotifyLogin(string token)
+    {
+        var identity = ParseClaimsFromJwt(token);
+        var user = new ClaimsPrincipal(identity);
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+    }
+
+    public void NotifyLogout()
+    {
+        var anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(anonymous)));
+    }
+
+    private ClaimsIdentity ParseClaimsFromJwt(string jwt)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var token = handler.ReadJwtToken(jwt);
+
+        var claims = token.Claims.ToList();
+
+        var roleClaims = token.Claims.Where(c => c.Type == JwtClaimTypes.Role);
+        foreach (var rc in roleClaims)
+        {
+            claims.Add(new Claim(JwtClaimTypes.Role, rc.Value));
+        }
+
+        return new ClaimsIdentity(claims, JwtClaimTypes.JwtTypes.AccessToken);
+    }
+}
--- Projects/Exadel.ReportHub/Exadel.ReportHub.Blazor/Program.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Blazor/Program.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -12,6 +13,9 @@ public static class Program
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddAuthorizationCore();
+        builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
